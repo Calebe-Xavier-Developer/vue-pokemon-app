116ecade3819c24056b5e0b3790f6b11af638644
Calebe-Xavier-Developer <57916053+Calebe-Xavier-Developer@users.noreply.github.com>
2024-10-05 02:52:25 -0300

    :ok_hand: feat: Completing pokeapi interactions and customizing home view with a little tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
   <div class="p-14">
-    <router-link class="flex justify-center text-4xl text-yellow-700" to="/">Pokemon Picker</router-link>
+    <router-link class="flex justify-center text-4xl text-yellow-400 font-semibold leading-[24.5px] text-nowrap" to="/">Pokemon Picker</router-link>
   </div>
   <router-view/>
 </template>
 
 <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    width: 100Vw;
+    height: 100vh;
+    background-color: #ff7d7d;
+  }
+
+  .custom-scrollbar::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+  }
+
+  .custom-scrollbar::-webkit-scrollbar-track {
+    background: none;
+  }
+
+  .custom-scrollbar::-webkit-scrollbar-thumb {
+    background-color: #ffd900;
+    border-radius: 28px;
+  }
+
+  .custom-scrollbar::-webkit-scrollbar-thumb:hover {
+    background-color: none;
+  }
+  .custom-scrollbar::-webkit-scrollbar-corner {
+    background: transparent;
+  } 
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h3>{{ $route.params.slug }}</h3>
+  <div class="w-full flex items-center justify-center">
+    <div v-if="pokemon" class="w-auto p-6 min-w-fit bg-cyan-200 mt-4 shadow-2xl flex justify-center flex-col items-center rounded-[24px]">
+      <h3 class="text-2xl text-green-900 font-bold uppercase p-2">{{ pokemon.name }}</h3>
+      <div class="flex justify-center">
+        <img v-if="pokemon.sprites.front_shiny" class="w-48" :src="pokemon.sprites.front_shiny" alt="">
+        <img v-if="pokemon.sprites.back_shiny" class="w-48" :src="pokemon.sprites.back_shiny" alt="">
+      </div>
+      <h3 class="text-orange-600 text-xl">Types</h3>
+      <div v-for="(type, idx) in pokemon.types" :key="idx">
+        <h5 class="text-blue-900">{{ type.type.name }}</h5>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { reactive, toRefs } from "vue";
+import { useRoute } from "vue-router";
+
+export default {
+  
+
+  setup() {
+    const route = useRoute();
+
+    const state = reactive({ pokemon: null })
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        state.pokemon = data;
+      })
+
+    return {...toRefs(state)}
+  }
+}
+</script>
